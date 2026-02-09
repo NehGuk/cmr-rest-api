a66094d92348f55f446b0842462b265857edb0e0
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-09 23:11:38 +0100

    Updates server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import { Express } from "express"
 const app: Express = express()
 const port = 8000
 
-app.listen(port, () => {
-  console.log(
-    `Hey! Server up and running on port ${port}. Grab some chocolate milk!`
-  )
+app.listen(port, (): void => {
+  console.log(`Hey! Server up and running on port ${port}.`)
 })
