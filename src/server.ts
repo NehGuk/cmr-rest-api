8a6bb71de056f7826fdd358902faccbb51fed19c
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-10 13:03:50 +0100

    Fixes typo

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app: Express = express()
 const port = 8000
 
 app.get("/", (req: Request, res: Response): void => {
-  res.json({ name: "Coccio", countryOfOrigin: "Denmark" })
+  res.json({ name: "Cocio", countryOfOrigin: "Denmark" })
 })
 app.listen(port, (): void => {
   console.log(`Hey! Server up and running on port ${port}.`)
