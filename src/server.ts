8a04a2aa0f0159f2f41104bc1964ee1a34584435
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-10 13:07:40 +0100

    Installs cors and @types/cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { Express, Request, Response } from "express"
+import cors from "cors"
 
 const app: Express = express()
 const port = 8000
 
+app.use(cors())
 app.get("/", (req: Request, res: Response): void => {
   res.json({ name: "Cocio", countryOfOrigin: "Denmark" })
 })
