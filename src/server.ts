44d404f2466c9f7de0dad2b6b7d9256d5ce05510
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-10 11:22:40 +0100

    Imports Request and Response types

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express"
-import { Express } from "express"
+import { Express, Request, Response } from "express"
 
 const app: Express = express()
 const port = 8000
 
+app.get("/", (req: Request, res: Response): void => {
+  res.json({})
+})
 app.listen(port, (): void => {
   console.log(`Hey! Server up and running on port ${port}.`)
 })
