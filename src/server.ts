ffbaf6fc7443904fe38ba450026fdd77aaa70415
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-12 10:25:38 +0100

    Adds countryOfOrigin request parameter

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,37 @@
 import express from "express"
 import { Express, Request, Response } from "express"
 import cors from "cors"
-import { chocolateMilkList } from "./data/data-sample.js"
+import { ChocolateMilk, chocolateMilkList } from "./data/data-sample"
 
 const app: Express = express()
 const port = 8000
 
 app.use(cors())
 
-app.get("/api", (req: Request, res: Response): void => {
-  res.json(chocolateMilkList)
-})
-
 app.get("/api/:id", (req: Request, res: Response): void => {
   const { id } = req.params
-  const filteredData = chocolateMilkList.find((item) => item.id === Number(id))
-  res.json(filteredData)
-  res.status(404).json({ message: "ID not found" })
+  if (id) {
+    const filteredData = chocolateMilkList.find(
+      (item) => item.id === Number(id)
+    )
+    res.json(filteredData)
+  } else {
+    res.status(404).json({ message: "ID not found" })
+  }
+})
+
+app.get("/api", (req: Request, res: Response): void => {
+  const { countryOfOrigin } = req.query
+  if (countryOfOrigin) {
+    const filteredData = chocolateMilkList.filter(
+      (item) =>
+        item.countryOfOrigin.toLowerCase() ===
+        countryOfOrigin.toString().toLowerCase()
+    )
+    res.json(filteredData)
+  } else {
+    res.json(chocolateMilkList)
+  }
 })
 
 app.use((req, res) => {
