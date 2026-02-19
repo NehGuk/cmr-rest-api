220ba4c6c6f2790e5abda12783ef780591716c4d
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-19 16:58:39 +0100

    Adds rate-limiter-flexible middleware for security

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,19 @@ import express from "express"
 import { Express } from "express"
 import cors from "cors"
 import { chocolateMilkRouter } from "./routes/routes"
+import {
+  apiRateLimiterMiddleware,
+  rateLimiterMiddleware,
+} from "./middleware/rateLimiter"
 
 const app: Express = express()
 const PORT = process.env.PORT || 8080
 
 app.use(cors())
+app.use(express.json())
+app.use(rateLimiterMiddleware)
 
-app.use("/api", chocolateMilkRouter)
+app.use("/api", apiRateLimiterMiddleware, chocolateMilkRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" })
