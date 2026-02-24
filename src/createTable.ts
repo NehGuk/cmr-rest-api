e7f755a096b1686474e9fca686fd52af17daed25
NehGuk <nurture.tumulus_0n@icloud.com>
2026-02-24 12:48:49 +0100

    Fixes node configuration hell?

--- src/createTable.ts
+++ src/createTable.ts
@@ -1,23 +1,24 @@
 import Database from "better-sqlite3"
-import path from "path"
+import path from "node:path"
 
-export async function createTable() {
+export function createTable() {
   const db = new Database(path.join("cmr-database.db"))
 
-  await db.exec(`
+  db.exec(`
     CREATE TABLE IF NOT EXISTS cmr (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT NOT NULL,
       description TEXT NOT NULL,
       country_of_origin TEXT NOT NULL,
-      rating INTEGER NOT NULL CHECK(rating >=0 AND rating <=5),
-      contains_coffee BOOLEAN NOT NULL DEFAULT FALSE,
-      is_hot-chocolate BOOLEAN NOT NULL DEFAULT FALSE,
+      rating INTEGER NOT NULL CHECK(rating >= 0 AND rating <= 5),
+      contains_coffee INTEGER NOT NULL DEFAULT 0,
+      is_hot_chocolate INTEGER NOT NULL DEFAULT 0,
       image TEXT NOT NULL,
-      created_at? TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
   `)
 
-  await db.close()
-  console.log("Yayyy! Table created successfully.")
+  db.close()
 }
+
+createTable()
